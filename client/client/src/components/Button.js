2f9295751b3ecb63e6ce3ce9eeb3b36435386924
spy2115 <wazzaroo@wp.pl>
2025-01-25 16:47:51 +0100

    styling changes

--- client/client/src/components/Button.js
+++ client/client/src/components/Button.js
@@ -1,6 +1,6 @@
 export default function Button({label}) {
     return (
-        <button className="px-6 py-2 bg-emerald-800 text-white rounded hover:bg-emerald-900">
+        <button className="px-6 py-2 bg-emerald-900 text-white rounded-lg hover:bg-emerald-950">
           {label}
         </button>
     )
--- client/client/src/components/list/ListElement.js
+++ client/client/src/components/list/ListElement.js
@@ -1,6 +1,6 @@
 export default function ListElement({date, topic, author, comments}) {
     return (
-        <div className="col-span-12 grid grid-cols-12 gap-4 bg-slate-800 px-4 py-2 rounded-3xl hover:bg-slate-700">
+        <div className="col-span-12 grid grid-cols-12 gap-4 bg-slate-800 px-4 py-2 rounded-3xl hover:bg-slate-700 border border-slate-600 hover:border-slate-500">
             <h1 className='text-white'> {date} </h1>
             <h1 className='text-white col-span-9'> {topic}  </h1>
             <h1 className='text-white'> {author} </h1>
